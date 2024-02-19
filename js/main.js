579beef5c255028831a834f2f5f50a697a08f7d8
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2024-02-19 19:17:30 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,505 @@
+let canvas, textbox, gl, shader, batcher, assetManager, skeletonRenderer;
+let mvp = new spine.webgl.Matrix4();
+
+let lastFrameTime;
+let spineData;
+
+let customScale = 1;
+let targetFps = 60;
+let bgmfile = '';
+let bgmvolume = 0;
+let bgm;
+let bufferColor = [0.3, 0.3, 0.3];
+
+let introAnimation, acceptingClick;
+let dialogBox = false;
+let currentVoiceline = 1;
+let mouseSelect = -1;
+let trackerID = -1;
+let untrackerID = -1;
+let unpetID = -1;
+let PPointX, PPointY, EPointX, EPointY;
+let TPoint, TEye;
+let flipped = false;
+
+let transpose = 1;
+
+const CHARACTER = 'CH0184';
+const BINARY_PATH = window.location.protocol === 'file:' ? location.href.replace('/index.html', `/assets/${CHARACTER}_home.skel`) : `../assets/${CHARACTER}_home.skel`;
+const ATLAS_PATH = window.location.protocol === 'file:' ? location.href.replace('/index.html', `/assets/${CHARACTER}_home.atlas`) : `../assets/${CHARACTER}_home.atlas`;
+const ATLAS = window.location.protocol === 'file:' ? location.href.replace('/index.html', `/assets/${CHARACTER}_00.skel`) : `../assets/${CHARACTER}_00.atlas`;
+const BINARY = window.location.protocol === 'file:' ? location.href.replace('/index.html', `/assets/${CHARACTER}_00.skel`) : `../assets/${CHARACTER}_00.skel`;
+const HAS_A = { eye: false, point: true };
+
+// All voicelines are manually timed for start point and duration. This may not be the most optimized solution, but works for all intents and purposes.
+// NO VOICELINES!!
+const AUDIO_DETAIL = [];
+
+const HITBOX = {
+	headpat: { xMin: 936, xMax: 1475, yMin: 225, yMax: 306 },
+	voiceline: { xMin: 0, xMax: 0, yMin: 0, yMax: 0 }
+};
+
+const INTRO_TIMEOUT = 14000;
+const HEADPAT_CLAMP = 30;
+const EYE_CLAMP_X = 200;
+const EYE_CLAMP_Y = EYE_CLAMP_X * (9 / 16);
+const HEADPAT_STEP = 5;
+const EYE_STEP = 10;
+
+let mousePos = { x: 0, y: 0 };
+let volume = 0.5;
+let mouseOptions = { voicelines: false, headpatting: true, mousetracking: true };
+
+// Helper clamp function. Math does not appear to have clamp() in this version.
+function clamp(num, min, max) {
+	return Math.min(Math.max(num, min), max);
+}
+
+// NOTE: X and Y appears to be inversely related from cursor position to bone adjustment.
+//       This behavior's reason is unknown, but it LOOKS right so leave it alone!
+function trackMouse() {
+	let adjX = (mousePos.x / canvas.width) - 0.5;
+	let adjY = (mousePos.y / canvas.height) - 0.5;
+	TEye.y = TEye.y - (Math.sign(adjX) * EYE_STEP);
+	TEye.x = TEye.x - (Math.sign(adjY) * EYE_STEP);
+	TEye.y = clamp(TEye.y, EPointY - (Math.abs(adjX) * EYE_CLAMP_X), EPointY + (Math.abs(adjX) * EYE_CLAMP_X));
+	TEye.x = clamp(TEye.x, EPointX - (Math.abs(adjY) * EYE_CLAMP_Y), EPointX + (Math.abs(adjY) * EYE_CLAMP_Y));
+}
+
+function untrackMouse() {
+	if (Math.abs(TEye.y - EPointY) <= EYE_STEP && Math.abs(TEye.x - EPointX) <= EYE_STEP) {
+		if (untrackerID != -1) {
+			TEye.y = EPointY;
+			TEye.x = EPointX;
+			clearInterval(untrackerID);
+			untrackerID = -1;
+			setTimeout(function () {
+				acceptingClick = true;
+			}, 500);
+		}
+	}
+	if (TEye.y > EPointY) TEye.y -= EYE_STEP;
+	if (TEye.y < EPointY) TEye.y += EYE_STEP;
+	if (TEye.x > EPointX) TEye.x -= EYE_STEP;
+	if (TEye.x < EPointX) TEye.x += EYE_STEP;
+}
+
+function unpet() {
+	if (Math.abs(TPoint.x - PPointX) <= HEADPAT_STEP && Math.abs(TPoint.y - PPointY) <= HEADPAT_STEP) {
+		if (unpetID != -1) {
+			TPoint.x = PPointX;
+			TPoint.y = PPointY;
+			clearInterval(unpetID);
+			unpetID = -1;
+			setTimeout(function () {
+				acceptingClick = true;
+			}, 500);
+		}
+	}
+	if (TPoint.y > PPointY) TPoint.y -= HEADPAT_STEP;
+	if (TPoint.y < PPointY) TPoint.y += HEADPAT_STEP;
+	if (TPoint.x > PPointX) TPoint.x -= HEADPAT_STEP;
+	if (TPoint.x < PPointX) TPoint.x += HEADPAT_STEP;
+}
+
+function playVoiceline() {
+	spineData.state.setEmptyAnimation(1, 1);
+	spineData.state.setEmptyAnimation(2, 1);
+	spineData.state.addAnimation(1, `Talk_0${currentVoiceline}_M`, false, 0);
+	spineData.state.addAnimation(2, `Talk_0${currentVoiceline}_A`, false, 0);
+	spineData.state.addEmptyAnimation(1, 0.5, 0);
+	spineData.state.addEmptyAnimation(2, 0.5, 0);
+
+	let trackDetails = AUDIO_DETAIL[currentVoiceline - 1];
+
+	setTimeout(function () {
+		acceptingClick = true;
+		currentVoiceline = clamp(currentVoiceline + 1, 0, AUDIO_DETAIL.length);
+	}, trackDetails.time);
+
+	for (let i = 0; i < trackDetails.count; i++) {
+		let track;
+		if (trackDetails.count == 1) track = new Audio(`./assets/audio/${CHARACTER}_MemorialLobby_${currentVoiceline}.ogg`);
+		else track = new Audio(`./assets/audio/${CHARACTER}_MemorialLobby_${currentVoiceline}_${(i + 1)}.ogg`);
+		track.volume = volume;
+		setTimeout(function () {
+			track.play();
+			if (dialogBox) {
+				textbox.innerHTML = trackDetails.dialog[i];
+				textbox.style.opacity = 1;
+				track.addEventListener('ended', function () {
+					textbox.style.opacity = 0;
+				});
+			}
+		}, trackDetails.startTimes[i])
+	}
+}
+
+// Hitbox Scaling
+function t(n, side) {
+	let d = { x: { length: 2880, mid: (canvas.width / 2) }, y: { length: 1620, mid: (canvas.height / 2) } }
+	n = d[side].mid - n;
+	n = (d[side].length / (transpose * 2)) - n;
+	n = (n - (d[side].length / (transpose * 2))) / customScale;
+	return (n + (d[side].length / (transpose * 2))) * transpose;
+}
+
+// -1 = [No Entry], 1 = Headpat, 2 = Voiceline, 3 = Eye Track
+function pressedMouse(x, y) {
+	tx = t(x, 'x');
+	ty = t(y, 'y');
+	if (tx > HITBOX.headpat.xMin && tx < HITBOX.headpat.xMax && ty > HITBOX.headpat.yMin && ty < HITBOX.headpat.yMax && mouseOptions.headpatting) {
+		spineData.state.setAnimation(1, 'Pat_01_M', false);
+		if (HAS_A.point) spineData.state.setAnimation(2, 'Pat_01_A', false);
+		mouseSelect = 1;
+	}
+	else if (tx > HITBOX.voiceline.xMin && tx < HITBOX.voiceline.xMax && ty > HITBOX.voiceline.yMin && ty < HITBOX.voiceline.yMax && mouseOptions.voicelines) {
+		mouseSelect = 2;
+	}
+	else if (mouseOptions.mousetracking) {
+		if (trackerID == -1) {
+			trackerID = setInterval(trackMouse, 20);
+		}
+		spineData.state.setEmptyAnimation(1, 0);
+		spineData.state.setEmptyAnimation(2, 0);
+		let eyetracking = spineData.state.addAnimation(1, 'Look_01_M', false, 0);
+		eyetracking.mixDuration = 0.2;
+		if (HAS_A.eye) {
+			let eyetracking2 = spineData.state.addAnimation(2, 'Look_01_A', false, 0);
+			eyetracking2.mixDuration = 0.2;
+		}
+		mousePos.x = x;
+		mousePos.y = y;
+		mouseSelect = 3;
+	}
+	else if (mouseSelect == -1) {
+		acceptingClick = true;
+	}
+}
+
+function movedMouse(x, y, deltaX, deltaY) {
+	switch (mouseSelect) {
+		case 1:
+			// Motion: Clockwise
+			if ((y < 810 && deltaY < 0) || (x >= 1440 && deltaX > 0)) {
+				TPoint.y = clamp(TPoint.y - HEADPAT_STEP, PPointY - HEADPAT_CLAMP, PPointY + HEADPAT_CLAMP);
+			}
+			else if ((y >= 810 && deltaY > 0) || (x < 1440 && deltaX < 0)) {
+				TPoint.y = clamp(TPoint.y + HEADPAT_STEP, PPointY - HEADPAT_CLAMP, PPointY + HEADPAT_CLAMP);
+			}
+			break;
+		case 2:
+			mouseSelect = -1;
+			acceptingClick = true;
+			break;
+		case 3:
+			mousePos.x = x;
+			mousePos.y = y;
+			break;
+		default:
+	}
+}
+
+function releasedMouse() {
+	switch (mouseSelect) {
+		case 1:
+			if (unpetID == -1) {
+				unpetID = setInterval(unpet, 20);
+			}
+			spineData.state.setAnimation(1, 'PatEnd_01_M', false);
+			spineData.state.setAnimation(2, 'PatEnd_01_A', false);
+			spineData.state.addEmptyAnimation(1, 0.5, 0);
+			spineData.state.addEmptyAnimation(2, 0.5, 0);
+			break;
+		case 2:
+			playVoiceline();
+			break;
+		case 3:
+			if (trackerID != -1) {
+				clearInterval(trackerID);
+				trackerID = -1;
+			}
+			if (untrackerID == -1) {
+				untrackerID = setInterval(untrackMouse, 20);
+			}
+			let eyetracking = spineData.state.setAnimation(1, 'LookEnd_01_M', false);
+			let eyetracking2 = spineData.state.setAnimation(2, 'LookEnd_01_A', false);
+			eyetracking.mixDuration = 0;
+			eyetracking2.mixDuration = 0;
+			spineData.state.addEmptyAnimation(1, 0.5, 0);
+			spineData.state.addEmptyAnimation(2, 0.5, 0);
+			break;
+		default:
+	}
+	mouseSelect = -1;
+}
+
+// adjusts mouse values for flipped canvas
+function setMouse(event) {
+	let ax = event.clientX;
+	let ay = event.clientY;
+	let mx = 1;
+	if (flipped) {
+		mx = -1;
+		ax = canvas.width - ax;
+	}
+
+	return { x: ax, y: ay, m: mx }
+}
+
+function livelyPropertyListener(name, val) {
+	if (name == "schemecolor") {
+		bufferColor = val.split(" ");
+	};
+	if (name == "alignmentfliph") flipped = val;
+	if (name == "scale") {
+		customScale = val;
+		resize();
+	};
+	if (name == "targetfps") targetFps = val;
+
+	if (name == "introanimation") introAnimation = val;
+
+	if (name == "mousetracking") mouseOptions.mousetracking = val;
+	if (name == "headpatting") mouseOptions.headpatting = val;
+	// if (props.voicelines) mouseOptions.voicelines = props.voicelines.value;
+	// if (props.voicevolume) volume = props.voicevolume.value / 100;
+	// if (props.showdialog) dialogBox = props.showdialog.value;
+	// if (props.dialogx) textbox.style.left = props.dialogx.value + '%';
+	// if (props.dialogy) textbox.style.top = props.dialogy.value + '%';
+
+	if (name == "bgmfile") {
+		bgmfile = 'file:///' + val.replace("%3A", "\:");
+	};
+	if (name == "bgmvolume") {
+		bgmvolume = val / 100;
+		if (bgm) bgm.volume = bgmvolume;
+	};
+}
+
+
+function init() {
+	textbox = document.getElementById('textbox');
+
+	canvas = document.getElementById('canvas');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	let config = { alpha: false, premultipliedAlpha: false };
+	gl = canvas.getContext('webgl', config) || canvas.getContext('experimental-webgl', config);
+	if (!gl) {
+		alert('WebGL is unavailable.');
+		return;
+	}
+
+	// Create a simple shader, mesh, model-view-projection matrix, SkeletonRenderer, and AssetManager.
+	shader = spine.webgl.Shader.newTwoColoredTextured(gl);
+	batcher = new spine.webgl.PolygonBatcher(gl);
+	mvp.ortho2d(0, 0, canvas.width - 1, canvas.height - 1);
+	skeletonRenderer = new spine.webgl.SkeletonRenderer(gl);
+	assetManager = new spine.webgl.AssetManager(gl);
+
+	// Load assets for use.
+	assetManager.loadBinary(BINARY_PATH);
+	assetManager.loadTextureAtlas(ATLAS_PATH);
+	assetManager.loadBinary(BINARY);
+	assetManager.loadTextureAtlas(ATLAS);
+
+	requestAnimationFrame(load);
+}
+
+// CITATION: http://esotericsoftware.com/spine-api-reference#
+// CITATION: http://en.esotericsoftware.com/forum/Spine-Unity-Making-the-arm-follow-the-mouse-7856
+function interactionLoad() {
+	// Touch_Point and Touch_Eye
+	TPoint = spineData.skeleton.findBone('Touch_Point');
+	TEye = spineData.skeleton.findBone('Touch_Eye');
+	PPointX = TPoint.x;
+	PPointY = TPoint.y;
+	EPointX = TEye.x;
+	EPointY = TEye.y;
+
+	downaction = canvas.addEventListener('mousedown', function (event) {
+		if (!acceptingClick) {
+			return;
+		}
+		acceptingClick = false;
+		let mouseData = setMouse(event);
+		pressedMouse(mouseData.x, mouseData.y);
+	});
+	upaction = canvas.addEventListener('mouseup', function () {
+		releasedMouse();
+	});
+	moveaction = canvas.addEventListener('mousemove', function (event) {
+		let mouseData = setMouse(event);
+		movedMouse(mouseData.x, mouseData.y, (event.movementX * mouseData.m), event.movementY);
+	});
+
+	if (!introAnimation) {
+		acceptingClick = true;
+	}
+	else {
+		setTimeout(function () {
+			acceptingClick = true;
+		}, INTRO_TIMEOUT);
+	}
+
+	return 1;
+}
+
+function load() {
+	// Wait until the AssetManager has loaded all resources, then load the skeletons.
+	if (assetManager.isLoadingComplete() && typeof introAnimation !== 'undefined') {
+		spineData = loadSpineData(BINARY_PATH, ATLAS_PATH, false);
+
+		// User Option to skip Intro Animation
+		if (introAnimation) {
+			spineData.state.addAnimation(0, 'Start_Idle_01', false);
+			setTimeout(() => {
+				spineData = loadSpineData(BINARY, ATLAS, false);
+				spineData.state.addAnimation(0, 'Start_Idle_01', false);
+			}, 2000)
+
+			setTimeout(() => {
+				spineData = loadSpineData(BINARY_PATH, ATLAS_PATH, false);
+				spineData.state.setAnimation(0, 'Start_Idle_02', false);
+				spineData.state.addAnimation(0, 'Idle_01', true, 0);
+				interactionLoad();
+				resize();
+			}, 5000)
+
+		}
+		spineData.state.addAnimation(0, 'Idle_01', true, 0);
+
+
+		interactionLoad();
+		resize();
+
+		// Plays BGM (if set)
+		bgm = new Audio(bgmfile);
+		bgm.volume = bgmvolume;
+		bgm.play();
+		bgm.addEventListener('ended', function () {
+			this.currentTime = 0;
+			this.play();
+		}, false);
+
+		lastFrameTime = Date.now() / 1000;
+		// Call render every frame.
+		requestAnimationFrame(render);
+	} else {
+		requestAnimationFrame(load);
+	}
+}
+
+function loadSpineData(binaryPath, atlasPath, premultipliedAlpha) {
+	// Load the texture atlas from the AssetManager.
+	let atlas = assetManager.get(atlasPath);
+
+	// Create a AtlasAttachmentLoader that resolves region, mesh, boundingbox and path attachments
+	let atlasLoader = new spine.AtlasAttachmentLoader(atlas);
+
+	// Create a SkeletonBinary instance for parsing the .skel file.
+	let skeletonBinary = new spine.SkeletonBinary(atlasLoader);
+
+	// Set the scale to apply during parsing, parse the file, and create a new skeleton.
+	skeletonBinary.scale = 1;
+	let skeletonData = skeletonBinary.readSkeletonData(assetManager.get(binaryPath));
+	let skeleton = new spine.Skeleton(skeletonData);
+	let bounds = calculateSetupPoseBounds(skeleton);
+
+	// Create an AnimationState, and set the initial animation in looping mode.
+	let animationStateData = new spine.AnimationStateData(skeleton.data);
+	animationStateData.defaultMix = 0.5;
+	let animationState = new spine.AnimationState(animationStateData);
+
+	// Pack everything up and return to caller.
+	return { skeleton: skeleton, state: animationState, bounds: bounds, premultipliedAlpha: premultipliedAlpha };
+}
+
+function calculateSetupPoseBounds(skeleton) {
+	skeleton.setToSetupPose();
+	skeleton.updateWorldTransform();
+	let offset = new spine.Vector2();
+	let size = new spine.Vector2();
+	skeleton.getBounds(offset, size, []);
+	return { offset: offset, size: size };
+}
+
+function render() {
+	let now = Date.now() / 1000;
+	let delta = now - lastFrameTime;
+
+	lastFrameTime = now;
+
+	gl.clearColor(bufferColor[0], bufferColor[1], bufferColor[2], 1);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// Apply the animation state based on the delta time.
+	let skeleton = spineData.skeleton;
+	let state = spineData.state;
+	let premultipliedAlpha = spineData.premultipliedAlpha;
+	state.update(delta);
+	state.apply(skeleton);
+	skeleton.updateWorldTransform();
+
+	// Bind the shader and set the texture and model-view-projection matrix.
+	shader.bind();
+	shader.setUniformi(spine.webgl.Shader.SAMPLER, 0);
+	shader.setUniform4x4f(spine.webgl.Shader.MVP_MATRIX, mvp.values);
+
+	// Start the batch and tell the SkeletonRenderer to render the active skeleton.
+	batcher.begin(shader);
+	skeletonRenderer.premultipliedAlpha = premultipliedAlpha;
+	skeletonRenderer.draw(batcher, skeleton);
+	batcher.end();
+
+	shader.unbind();
+
+	// throttle fps
+	let elapsed = Date.now() / 1000 - now;
+	let targetFrameTime = 1 / targetFps;
+	let delay = Math.max(targetFrameTime - elapsed, 0) * 1000;
+
+	setTimeout(() => {
+		requestAnimationFrame(render);
+	}, delay);
+}
+
+function resize() {
+	let w = canvas.clientWidth;
+	let h = canvas.clientHeight;
+	if (canvas.width != w || canvas.height != h) {
+		canvas.width = w;
+		canvas.height = h;
+	}
+
+	// Set values to position skeleton to center of canvas.
+	// Will always attempt to Fit to Fill while maintaining aspect ratio. As a result, a scale of [1] will mean different things across various device resolutions.
+	let centerX = 0;
+	let centerY = 900;
+	let wr = canvas.width / 2880;
+	let hr = canvas.height / 1620;
+	let width = (2880 / customScale);
+	let height = (1620 / customScale);
+
+	if (wr < hr) {
+		width = height * (canvas.width / canvas.height);
+
+		transpose = 1620 / canvas.height;
+	}
+	else if (wr > hr) {
+		height = width * (canvas.height / canvas.width);
+
+		transpose = 2880 / canvas.width;
+	}
+	else {
+		transpose = 1620 / canvas.height;
+	}
+
+	mvp.ortho2d(centerX - width / 2, centerY - height / 2, width, height);
+	gl.viewport(0, 0, canvas.width, canvas.height);
+}
+
+init();
